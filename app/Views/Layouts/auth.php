1033ca82e9e2984ac1e19143b5887b34fdd28b3b
unknown <bhuniajayant@gmail.com>
2022-12-31 14:28:49 +0530

    code push

---
+++ app/Views/Layouts/auth.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Eduport - LMS, Education and Course Theme</title>
+
+	<!-- Meta Tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="Webestica.com">
+	<meta name="description" content="Eduport- LMS, Education and Course Theme">
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="<?= base_url() ?>/assets/images/favicon.ico">
+
+	<!-- Google Font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&amp;family=Roboto:wght@400;500;700&amp;display=swap">
+
+	<!-- Plugins CSS -->
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/vendor/font-awesome/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css">
+
+	<!-- Theme CSS -->
+	<link id="style-switch" rel="stylesheet" type="text/css" href="<?= base_url() ?>/assets/css/style.css">
+
+</head>
+
+<body>
+
+	<!-- ==================================================== -->
+	<!-- **************** MAIN CONTENT START **************** -->
+	<!-- ==================================================== -->
+
+	<?= $this->renderSection('content') ?>
+
+	<!-- ==================================================== -->
+	<!-- **************** MAIN CONTENT END ****************** -->
+	<!-- ==================================================== -->
+
+	<!-- Back to top -->
+<div class="back-top"><i class="bi bi-arrow-up-short position-absolute top-50 start-50 translate-middle"></i></div>
+
+<!-- Bootstrap JS -->
+<script src="<?= base_url() ?>/assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Template Functions -->
+<script src="<?= base_url() ?>/assets/js/functions.js"></script>
+
+</body>
+
+<!-- Mirrored from eduport.webestica.com/sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 23 Mar 2022 09:52:57 GMT -->
+</html>
